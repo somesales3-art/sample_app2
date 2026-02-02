04d269a6f6d1c8011d5143d9b6cb2e4b00311f5e
some <somesales3@gmail.com>
2026-02-02 10:16:20 +0000

    7章の保存し忘れ

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -20,4 +20,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     assert_select 'div.alert'
   end
+
+  test 'ログイン情報が正しいのかの確認' do
+    assert_difference 'User.count', 1 do
+      post users_path, params: {
+        user: {
+          name: 'Example User',
+          email: 'user@example.com',
+          password: 'password',
+          password_confirmation: 'password'
+        }
+      }
+      follow_redirect!
+      assert_template 'users/show'
+    end
+  end
 end
